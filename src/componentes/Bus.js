a01f0f448017a8dd361809a6c0cffd6c2717cdd3
Santiago Arias <santyac27@gmail.com>
2021-03-16 22:19:49 +0100

    Correccion de warnings al usar useEffect

--- src/componentes/Bus.js
+++ src/componentes/Bus.js
@@ -6,30 +6,22 @@ const Bus = props => {
   const { paradas } = props;
 
   const [algo, setAlgo] = useState("0px");
-  let valueTop = 0;
-  let counter = 0;
-
-  const desplazarBus = () => {
-    valueTop = valueTop - 30;
-    counter++;
-    setAlgo(`${valueTop}px`);
-  };
-
-  const resetBuses = () => {
-    valueTop = 0;
-    counter = 0;
-    setAlgo("0px");
-  };
 
   useEffect(() => {
+    let valueTop = 0;
+    let counter = 0;
     const interval = setInterval(() => {
-      desplazarBus();
+      valueTop = valueTop - 30;
+      counter++;
+      setAlgo(`${valueTop}px`);
       if (counter === paradas.length) {
-        resetBuses();
+        valueTop = 0;
+        counter = 0;
+        setAlgo("0px");
       }
     }, 2000);
     return () => clearInterval(interval);
-  }, []);
+  }, [paradas.length]);
 
   return (
     paradas.map((parada) =>
